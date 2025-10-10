b945b732aaf033e4cd3a5a9a9c0a953e6f32779e
Lppm Admin <adminlppm@srv944780.hstgr.cloud>
2025-10-10 13:42:24 +0000

    astaga

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 // vite.config.js
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // <-- 1. Impor plugin baru
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path'; // <-- Impor modul 'path' dari Node.js
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- 2. Tambahkan plugin di sini
+    tailwindcss(),
   ],
+  build: {
+    // Tentukan direktori output build di sini
+    outDir: '/var/www/pks-frontend',
+
+    // Opsi ini akan membersihkan direktori output sebelum setiap build
+    emptyOutDir: true, 
+  }
 });
